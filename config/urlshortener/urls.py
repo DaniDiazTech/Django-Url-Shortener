449b120f1ea33184dc06dab1aa4e61016dd280d7
Daniel <danieldiazgonza52@gmail.com>
2021-05-06 23:26:44 -0500

    {Added} Views and Urls

--- config/urlshortener/urls.py
+++ config/urlshortener/urls.py
@@ -5,11 +5,12 @@ Urls for shortener app urlshortener/urls.py
 from django.urls import path
 
 # Import the home view
-from .views import home_view
+from .views import home_view, redirect_url_view
 
 appname = "shortener"
 
 urlpatterns = [
     # Home view
-    path("", home_view, name="home")
+    path('', home_view, name='home'),
+    path('<str:shortened_part>', redirect_url_view, name='redirect'),
 ]
--- config/urlshortener/views.py
+++ config/urlshortener/views.py
@@ -1,12 +1,61 @@
 '''
 Shortener views
 '''
-from django.shortcuts import render, get_object_or_404 # We will use it later
+from django.shortcuts import render # We will use it later
 
-from django.http import HttpResponse 
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+
+
+# Model
+from .models import Shortener
+
+# Custom form
+
+from .forms import ShortenerForm
 
 # Create your views here.
 
 def home_view(request):
     
-    pass
+    template = 'urlshortener/home.html'
+
+    # template = 'urlshortener/success.html'
+    
+    context = {}
+
+    # Empty form
+    context['form'] = ShortenerForm()
+
+    if request.method == 'GET':
+        return render(request, template, context)
+
+    elif request.method == 'POST':
+
+        used_form = ShortenerForm(request.POST)
+
+        if used_form.is_valid():
+            
+            shortened_object = used_form.save()
+
+            new_url = request.build_absolute_uri('/') + shortened_object.short_url
+            
+            long_url = shortened_object.long_url 
+             
+            context['new_url']  = new_url
+            context['long_url'] = long_url
+             
+            return render(request, template, context)
+
+def redirect_url_view(request, shortened_part):
+
+    try:
+        shortener = Shortener.objects.get(short_url=shortened_part)
+
+        shortener.times_followed += 1        
+
+        shortener.save()
+        
+        return HttpResponseRedirect(shortener.long_url)
+        
+    except:
+        raise Http404('Sorry this link is broken :(')
